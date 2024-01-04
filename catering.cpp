81f260c3c20ae69e2a2ab6714eeaf75fa3d709cf
wildanniam <wildanniam4@gmail.com>
2024-01-04 07:19:10 +0700

    Features App

---
+++ catering.cpp
@@ -0,0 +1,313 @@
+//
+//  catering.cpp
+//  catering
+//
+//  Created on 31/12/23.
+//
+
+#include <iostream>
+#include "catering.h"
+
+void createCateringList(CateringList &list) {
+    first(list) = NULL;
+}
+
+void createFoodMenuList(FoodMenuList &list) {
+    first(list) = NULL;
+}
+
+CateringAddress createCateringElement(CateringInfoType cateringData) {
+    CateringAddress catering = new CateringElement;
+    info(catering) = cateringData;
+    next(catering) = NULL;
+    firstChild(catering) = NULL;
+    return catering;
+}
+
+FoodMenuAddress createFoodMenuElement(FoodMenuInfoType foodMenuData) {
+    FoodMenuAddress foodMenu = new FoodMenuElement;
+    info(foodMenu) = foodMenuData;
+    next(foodMenu) = NULL;
+    prev(foodMenu) = NULL;
+    return foodMenu;
+}
+
+RelationAddress createRelationElement() {
+    RelationAddress relation = new RelationElement;
+    next(relation) = NULL;
+    foodMenu(relation) = NULL;
+    return relation;
+}
+
+void insertCatering(CateringList &list, CateringAddress catering) { // 1) Insert data parent dari depan/belakang
+    if (first(list) == NULL){
+        first(list) = catering;
+    } else {
+        CateringAddress iteration = first(list);
+        while(next(iteration) != NULL) {
+            iteration = next(iteration);
+        }
+
+        next(iteration) = catering;
+    }
+}
+
+void insertFoodMenu(FoodMenuList &list, FoodMenuAddress foodMenu) { // 6) Menambahkan data child
+    if (first(list) == NULL){
+        first(list) = foodMenu;
+    } else {
+        FoodMenuAddress iteration = first(list);
+        while(next(iteration) != NULL) {
+            iteration = next(iteration);
+        }
+
+        prev(foodMenu) = iteration;
+        next(iteration) = foodMenu;
+    }
+}
+
+void connectCateringToFoodMenu(CateringAddress &catering, FoodMenuAddress &foodMenu) { // 7) Menghubungkan data parent ke data child
+    RelationAddress relation = createRelationElement();
+    info(foodMenu).totalOrdered = info(foodMenu).totalOrdered + 1;
+    foodMenu(relation) = foodMenu;
+
+    if(firstChild(catering) == NULL) {
+        firstChild(catering) = relation;
+    } else {
+        RelationAddress iteration = firstChild(catering);
+        while(next(iteration) != NULL) {
+            iteration = next(iteration);
+        }
+        next(iteration) = relation;
+    }
+}
+
+void deleteCatering(CateringList &list, CateringAddress &catering) { // 3) Menghapus data parent beserta relasinya
+    if(first(list) == catering) {
+        first(list) = next(first(list));
+    } else {
+        CateringAddress iteration = first(list);
+        while(next(iteration) != catering) {
+            iteration = next(iteration);
+        }
+
+        next(iteration) = next(next(iteration));
+        firstChild(iteration) = NULL;
+    }
+}
+
+void deleteFoodMenu(FoodMenuList &list, FoodMenuAddress &foodMenu) {
+    if(first(list) == foodMenu) {
+        first(list) = next(first(list));
+    } else {
+        FoodMenuAddress iteration = first(list);
+        while(next(iteration) != foodMenu) {
+            iteration = next(iteration);
+        }
+
+        next(iteration) = next(next(iteration));
+    }
+}
+
+void deleteFoodMenuInCatering(CateringAddress &catering, FoodMenuAddress &foodMenu) { // 10) Menghapus data child pada parent tertentu beserta relasinya
+    if(foodMenu(firstChild(catering)) == foodMenu) {
+        firstChild(catering) = next(firstChild(catering));
+    }
+
+    RelationAddress iteration = firstChild(catering);
+
+    while(iteration != NULL) {
+        if(next(iteration) != NULL) {
+            if(foodMenu(next(iteration)) == foodMenu) {
+                next(iteration) = next(next(iteration));
+            }
+        }
+
+        iteration = next(iteration);
+    }
+}
+
+CateringAddress findCatering(CateringList list, int id) { // 4) Mencari data parent
+    if(info(first(list)).id == id) {
+        return first(list);
+    }
+
+    CateringAddress catering = NULL;
+    CateringAddress iteration = first(list);
+
+    while(iteration != NULL && catering == NULL) {
+        if(info(iteration).id == id) {
+            catering = iteration;
+        }
+
+        iteration = next(iteration);
+    }
+
+    return catering;
+}
+
+FoodMenuAddress findFoodMenu(FoodMenuList list, int id) { // 5) Mencari data child
+    if(info(first(list)).id == id) {
+        return first(list);
+    }
+
+    FoodMenuAddress foodMenu = NULL;
+    FoodMenuAddress iteration = first(list);
+
+    while(iteration != NULL && foodMenu == NULL) {
+        if(info(iteration).id == id) {
+            foodMenu = iteration;
+        }
+
+        iteration = next(iteration);
+    }
+
+    return foodMenu;
+}
+
+FoodMenuAddress findFoodMenuInCatering(CateringAddress catering, int foodMenuId) { // 9) Mencari data child pada parent tertentu
+    if(firstChild(catering) == NULL) {
+        return NULL;
+    } else if(info(foodMenu(firstChild(catering))).id == foodMenuId) {
+        return foodMenu(firstChild(catering));
+    } else {
+        FoodMenuAddress foodMenu = NULL;
+        RelationAddress iteration = firstChild(catering);
+        while(iteration != NULL) {
+            if(info(foodMenu(iteration)).id == foodMenuId) {
+                foodMenu = foodMenu(iteration);
+            }
+            iteration = next(iteration);
+        }
+
+        return foodMenu;
+    }
+}
+
+int getTotalFoodMenuInCatering(CateringAddress catering) { // 11) Menghitung jumlah data child dari parent tertentu
+    int total = 0;
+
+    if(firstChild(catering) == NULL) {
+        return 0;
+    }
+
+    RelationAddress iteration = firstChild(catering);
+    while(iteration != NULL) {
+        total = total + 1;
+        iteration = next(iteration);
+    }
+
+    return total;
+}
+
+int getMaxCateringID(CateringList list) {
+    if(first(list) == NULL) {
+        return 0;
+    }
+
+    int max = 0;
+    CateringAddress iteration = first(list);
+    while(iteration != NULL) {
+        if(info(iteration).id > max) {
+            max = info(iteration).id;
+        }
+        iteration = next(iteration);
+    }
+
+    return max;
+}
+
+int getMaxFoodMenuID(FoodMenuList list) {
+    if(first(list) == NULL) {
+        return 0;
+    }
+
+    int max = 0;
+    FoodMenuAddress iteration = first(list);
+    while(iteration != NULL) {
+        if(info(iteration).id > max) {
+            max = info(iteration).id;
+        }
+        iteration = next(iteration);
+    }
+
+    return max;
+}
+
+void showCateringList(CateringList list) { // 2) Show all data parent
+    CateringAddress iteration = first(list);
+
+    if(iteration == NULL) {
+        cout << "Data catering kosong!" << endl;
+    } else {
+        while(iteration != NULL) {
+            cout << "ID : " << info(iteration).id << endl;
+            cout << "Nama : " << info(iteration).name << endl;
+            cout << "Harga : " << info(iteration).price << endl;
+
+            iteration = next(iteration);
+        }
+    }
+}
+
+void showFoodMenuList(FoodMenuList list) {
+    FoodMenuAddress iteration = first(list);
+
+    if(iteration == NULL) {
+        cout << "Data menu kosong!" << endl;
+    } else {
+        while(iteration != NULL) {
+            cout << "ID : " << info(iteration).id << endl;
+            cout << "Nama : " << info(iteration).name << endl;
+            cout << "Total Terjual : " << info(iteration).totalOrdered << endl;
+
+            iteration = next(iteration);
+        }
+    }
+}
+
+void showCateringListWithFoodMenu(CateringList list) { // 8) Menampilkan seluruh data parent beserta childnya
+    CateringAddress cateringIteration = first(list);
+
+    if(cateringIteration == NULL) {
+        cout << "Data catering kosong!" << endl;
+    } else {
+        while(cateringIteration != NULL) {
+            cout << "ID : " << info(cateringIteration).id << endl;
+            cout << "Nama : " << info(cateringIteration).name << endl;
+            cout << "Harga : " << info(cateringIteration).price << endl;
+
+            cout << "Menu : " << endl;
+            if(firstChild(cateringIteration) == NULL) {
+                cout << "Tidak ada menu" << endl;
+            } else {
+                showFoodMenuInRelation(firstChild(cateringIteration));
+            }
+            cateringIteration = next(cateringIteration);
+        }
+    }
+}
+
+void showFoodMenuInRelation(RelationAddress relation) {
+    while(relation != NULL) {
+        cout << info(foodMenu(relation)).id <<  ") " << info(foodMenu(relation)).name << endl;
+        relation = next(relation);
+    }
+}
+
+FoodMenuAddress showHightesOrdered(FoodMenuList list) { // Soal studi kasus
+    if(first(list) == NULL) {
+        return NULL;
+    }
+
+    FoodMenuAddress max = first(list);
+    FoodMenuAddress iteration = first(list);
+    while(iteration != NULL) {
+        if(info(iteration).totalOrdered > info(max).totalOrdered) {
+            max = iteration;
+        }
+        iteration = next(iteration);
+    }
+
+    return max;
+}
---
+++ main.cpp
@@ -0,0 +1,213 @@
+//
+//  main.cpp
+//  catering
+//
+//  Created on 16/12/23.
+//
+
+#include <iostream>
+#include "catering.h"
+
+using namespace std;
+
+int main() {
+    CateringList cateringList;
+    FoodMenuList foodMenuList;
+
+    createCateringList(cateringList);
+    createFoodMenuList(foodMenuList);
+
+    int menu;
+
+    do {
+        cout << "========== Selamat datang di layanan catering ==========" << endl;
+        cout << "1. Tambah Catering" << endl;
+        cout << "2. Tambah Menu Makanan" << endl;
+        cout << "3. Hubungkan Catering dan Menu Makanan" << endl;
+        cout << "4. Hapus Data Catering" << endl;
+        cout << "5. Hapus Data Menu Makanan" << endl;
+        cout << "6. Tampilkan Data Catering" << endl;
+        cout << "7. Tampilkan Data Makanan" << endl;
+        cout << "8. Tampilkan Data Catering Beserta Makanan" << endl;
+        cout << "9. Tampilkan Makanan Terbanyak yang dijual" << endl;
+        cout << "10. Cari Data Catering" << endl;
+        cout << "11. Cari Data Makanan" << endl;
+        cout << "12. Cari Data Menu Makanan Pada Catering" << endl;
+        cout << "13. Tampilkan jumlah makanan dalam catering" << endl;
+        cout << "0. Keluar" << endl;
+        cout << "Pilih menu: ";
+        cin >> menu;
+
+        // Menu process
+        if(menu == 1) {
+            CateringInfoType cateringInfo;
+            cateringInfo.id = getMaxCateringID(cateringList) + 1;
+            cout << "Masukkan Nama Catering: ";
+            cin.ignore();
+            getline(cin, cateringInfo.name);
+            cout << "Masukkan Harga: ";
+            cin >> cateringInfo.price;
+
+            insertCatering(cateringList, createCateringElement(cateringInfo));
+            cout << "Catering berhasil ditambahkan." << endl;
+        } else if(menu == 2) {
+            FoodMenuInfoType foodMenuInfo;
+            foodMenuInfo.id = getMaxFoodMenuID(foodMenuList) + 1;
+            foodMenuInfo.totalOrdered = 0;
+            cout << "Masukkan Nama Menu Makanan: ";
+            cin.ignore();
+            getline(cin, foodMenuInfo.name);
+
+            insertFoodMenu(foodMenuList, createFoodMenuElement(foodMenuInfo));
+            cout << "Menu makanan berhasil ditambahkan." << endl;
+        } else if(menu == 3) {
+            int cateringId;
+            cout << "Masukkan ID Catering: ";
+            cin >> cateringId;
+
+            CateringAddress catering = findCatering(cateringList, cateringId);
+
+            if(catering == NULL) {
+                cout << "Data Catering tidak ditemukan!" << endl;
+                continue;
+            }
+
+            int foodId;
+            cout << "Masukkan ID Makanan: ";
+            cin >> foodId;
+
+            FoodMenuAddress foodMenu = findFoodMenu(foodMenuList, foodId);
+
+            if(foodMenu == NULL) {
+                cout << "Data Makanan tidak ditemukan!" << endl;
+                continue;
+            }
+
+            connectCateringToFoodMenu(catering, foodMenu);
+
+            cout << "Berhasil menguhubungkan Catering dengan Makanan" << endl;
+
+        } else if(menu == 4) {
+            int id;
+            cout << "Masukkan ID yang ingin dihapus: ";
+            cin >> id;
+            cout << id << endl;
+
+            CateringAddress deletedCatering = findCatering(cateringList, id);
+
+            if(deletedCatering == NULL) {
+                cout << "ID Tidak ditemukan!" << endl;
+            } else {
+                deleteCatering(cateringList, deletedCatering);
+                cout << "Berhasil menghapus catering" << endl;
+            }
+        } else if(menu == 5) {
+            int id;
+            cout << "Masukkan ID yang ingin dihapus: ";
+            cin >> id;
+            cout << id << endl;
+
+            FoodMenuAddress deletedFoodMenu = findFoodMenu(foodMenuList, id);
+
+            if(deletedFoodMenu == NULL) {
+                cout << "ID Tidak ditemukan!" << endl;
+            } else {
+                CateringAddress catering = first(cateringList);
+                while(catering != NULL) {
+                    FoodMenuAddress foodMenu = findFoodMenuInCatering(catering, id);
+                    if(foodMenu != NULL) {
+                        deleteFoodMenuInCatering(catering, foodMenu);
+                    }
+                    catering = next(catering);
+                }
+
+                deleteFoodMenu(foodMenuList, deletedFoodMenu);
+                cout << "Berhasil menghapus makanan" << endl;
+            }
+        } else if(menu == 6) {
+            showCateringList(cateringList);
+        } else if(menu == 7) {
+            showFoodMenuList(foodMenuList);
+        } else if(menu == 8) {
+            showCateringListWithFoodMenu(cateringList);
+        } else if(menu == 9) {
+            FoodMenuAddress foodMenu = showHightesOrdered(foodMenuList);
+
+            if(foodMenu == NULL) {
+                cout << "Data makanan kosong!" << endl;
+            } else {
+                cout << "Makanan terbanyak terjual adalah: " << info(foodMenu).name << endl;
+            }
+            cout << "10. Cari Data Catering" << endl;
+            cout << "11. Cari Data Makanan" << endl;
+            cout << "12. Cari Data Menu Makanan Pada Catering" << endl;
+        } else if(menu == 10) {
+            int id;
+            cout << "Masukkan ID yang dicari: ";
+            cin >> id;
+            cout << id << endl;
+
+            CateringAddress catering = findCatering(cateringList, id);
+            if(catering == NULL) {
+                cout << "Data tidak ditemukan!" << endl;
+            } else {
+                cout << "Data ditemukan dengan nama: " << info(catering).name << endl;
+            }
+        } else if(menu == 11) {
+            int id;
+            cout << "Masukkan ID yang dicari: ";
+            cin >> id;
+            cout << id << endl;
+
+            FoodMenuAddress foodMenu = findFoodMenu(foodMenuList, id);
+            if(foodMenu == NULL) {
+                cout << "Data tidak ditemukan!" << endl;
+            } else {
+                cout << "Data ditemukan dengan nama: " << info(foodMenu).name << endl;
+            }
+        } else if(menu == 12) {
+            int id;
+            cout << "Masukkan ID Catering: ";
+            cin >> id;
+            cout << id << endl;
+
+            CateringAddress catering = findCatering(cateringList, id);
+            if(catering == NULL) {
+                cout << "Data tidak ditemukan!" << endl;
+                continue;
+            }
+
+            cout << "Masukkan ID Makanan: ";
+            cin >> id;
+            cout << id << endl;
+
+            FoodMenuAddress foodMenu = findFoodMenuInCatering(catering, id);
+            if(foodMenu == NULL) {
+                cout << "Data tidak ditemukan!" << endl;
+            } else {
+                cout << "Data ditemukan dengan nama: " << info(foodMenu).name << endl;
+            }
+        } else if(menu == 13) {
+            int id;
+            cout << "Masukkan ID Catering: ";
+            cin >> id;
+            cout << id << endl;
+
+            CateringAddress catering = findCatering(cateringList, id);
+            if(catering == NULL) {
+                cout << "Data tidak ditemukan!" << endl;
+                continue;
+            }
+
+            int total = getTotalFoodMenuInCatering(catering);
+            cout << "Total menu berjumlah: " << total << endl;
+        } else {
+            cout << "Menu Tidak ditemukan!" << endl;
+        }
+
+    } while (menu != 0);
+
+    cout << "Berhasil keluar aplikasi" << endl;
+
+    return 0;
+}
